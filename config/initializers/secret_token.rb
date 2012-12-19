d66a4fdc59f8fdccfaca93f0317cc3678143cfac
joeyevoli <joe.yevoli@gmail.com>
2012-12-19 16:48:09 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nyst::Application.config.secret_token = '874dd7b2cbb82b694d63b328b895bc5ee0e17fe41b2508ec2735ffa09f9b85a47db9c804f61695f7c6131dbe47256063b451cf87460fe4384aebf113a85e5b57'
